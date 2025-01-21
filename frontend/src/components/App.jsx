8c14cababb9ad02a500b43f8f44a61620b0f06f3
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-01-21 23:56:47 +0300

    feat: add sections below the landing page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,15 @@
 import "../styles/App.css";
+import "../styles/Sections.css";
 import { Header } from "./Header.jsx";
 import { Landing } from "./Landing.jsx";
+import { Sections } from "./Sections.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Landing />
+      <Sections />
     </>
   );
 }
