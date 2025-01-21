ab748c8c51ee7907ea19d4d2262cf3b0191ececb
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-01-21 21:09:54 +0300

    feat: add two headers for large screens and small screens

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,35 +1,74 @@
 import logo from "../images/header-logo.png";
+import menuIcon from "../images/menu-icon.svg";
 
 export function Header() {
   return (
     <header>
-      <div>
-        <img src={logo} />
-      </div>
-      <div className="left-header">
+      <div className="small-screens">
+        <div className="menu-icon-container">
+          <img src={menuIcon} />
+          <div className="menu-items">
         <div className="about">
-          <p>About</p>
+            <p>About</p>
+          </div>
+          <div className="gallery">
+            <p>Gallery</p>
+          </div>
+          <div className="news">
+            <p>News</p>
+          </div>
+          <div className="events">
+            <p>Events</p>
+          </div>
+          <div className="login">
+            <p>Results</p>
+          </div>
+          <div className="login">
+            <p>Projects</p>
+          </div>
         </div>
-        <div className="gallery">
-          <p>Gallery</p>
         </div>
-        <div className="news">
-          <p>News</p>
+        <div className="mithiu-logo">
+          <img src={logo} />
         </div>
-        <div className="events">
-          <p>Events</p>
+        <div className="logins">
+          <div className="login">
+            <p>Login</p>
+          </div>
+          <div className="cta-button">
+            <span>Register</span>
+          </div>
         </div>
-        <div className="login">
-          <p>Results</p>
+      </div>
+      <div className="large-screens">
+        <div>
+          <img src={logo} />
         </div>
-        <div className="login">
-          <p>Projects</p>
-        </div>
-        <div className="login">
-          <p>Login</p>
-        </div>
-        <div className="cta-button">
-          <span>Register</span>
+        <div className="left-header">
+          <div className="about">
+            <p>About</p>
+          </div>
+          <div className="gallery">
+            <p>Gallery</p>
+          </div>
+          <div className="news">
+            <p>News</p>
+          </div>
+          <div className="events">
+            <p>Events</p>
+          </div>
+          <div className="login">
+            <p>Results</p>
+          </div>
+          <div className="login">
+            <p>Projects</p>
+          </div>
+          <div className="login">
+            <p>Login</p>
+          </div>
+          <div className="cta-button">
+            <span>Register</span>
+          </div>
         </div>
       </div>
     </header>
