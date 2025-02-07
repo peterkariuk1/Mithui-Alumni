af77beda5933c8005130e2571043227c1fc51027
v!g1l4n73 <vincegichomo704@gmail.com>
2025-02-07 18:12:36 +0300

    Completed Results Operations

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -2,8 +2,66 @@ import { Link } from "react-router-dom";
 import HomeIcon from "../images/homeicon.png";
 import RightIcon from "../images/righticon.svg";
 import "../styles/Pages.css";
+import { useState } from "react";
+import { db } from "../../firebaseConfig";
+import { setDoc, doc } from "firebase/firestore";
 
 export function AdminPage() {
+  const [year, setYear] = useState('')
+  const [mean, setMean] = useState('')
+  const [gradeA, setGradeA] = useState('')
+  const [gradeAminus, setGradeAminus] = useState('')
+  const [gradeBplus, setGradeBplus] = useState('')
+  const [gradeB, setGradeB] = useState('')
+  const [gradeBminus, setGradeBminus] = useState('')
+  const [gradeCplus, setGradeCplus] = useState('')
+  const [gradeC, setGradeC] = useState('')
+  const [gradeCminus, setGradeCminus] = useState('')
+  const [gradeDplus, setGradeDplus] = useState('')
+  const [gradeD, setGradeD] = useState('')
+  const [gradeDminus, setGradeDminus] = useState('')
+  const [gradeE, setGradeE] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await setDoc(doc(db, "results", year), {
+        year,
+        mean,
+        gradeA,
+        gradeAminus,
+        gradeBplus,
+        gradeB,
+        gradeBminus,
+        gradeCplus,
+        gradeC,
+        gradeCminus,
+        gradeDplus,
+        gradeD,
+        gradeDminus,
+        gradeE,
+      });
+      alert("Results added successfully");
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setYear('');
+      setMean('');
+      setGradeA('');
+      setGradeAminus('');
+      setGradeBplus('');
+      setGradeB('');
+      setGradeBminus('');
+      setGradeCplus('');
+      setGradeC('');
+      setGradeCminus('');
+      setGradeDplus('');
+      setGradeD('');
+      setGradeDminus('');
+      setGradeE('');
+    }
+  };  
+
   return (
     <div className="about-page">
       <section className="top">
@@ -27,60 +85,61 @@ export function AdminPage() {
         {/* Input fields for admin to add results */}
         <div className="results-inputs-container">
           <h1>Add Results:</h1>
-          <div className="input-results">
-            <input type="number" placeholder="Enter Year" />
-            <input type="number" placeholder="Enter Mean Grade" />
+          <form onSubmit={handleSubmit} className="input-results">
+            <input type="text" placeholder="Enter Year" value={year} onChange={(e) => setYear(e.target.value)} />
+            <input type="text" step="any" placeholder="Enter Mean Grade" value={mean} onChange={(e) => setMean(e.target.value)} />
             <div className="grades-inputs-container">
               <div>
                 <p>Grade A:</p>
-                <input type="number" placeholder="Grade A" />
+                <input type="text" placeholder="Grade A" value={gradeA} onChange={(e) => setGradeA(e.target.value)} />
               </div>
               <div>
                 <p>Grade A-:</p>
-                <input type="number" placeholder="Grade A-" />
+                <input type="text" placeholder="Grade A-" value={gradeAminus} onChange={(e) => setGradeAminus(e.target.value)} />
               </div>
               <div>
                 <p>Grade B+:</p>
-                <input type="number" placeholder="Grade B+" />
+                <input type="text" placeholder="Grade B+" value={gradeBplus} onChange={(e) => setGradeBplus(e.target.value)} />
               </div>
               <div>
                 <p>Grade B:</p>
-                <input type="number" placeholder="Grade B" />
+                <input type="text" placeholder="Grade B" value={gradeB} onChange={(e) => setGradeB(e.target.value)} />
               </div>
               <div>
                 <p>Grade B-:</p>
-                <input type="number" placeholder="Grade B-" />
+                <input type="text" placeholder="Grade B-" value={gradeBminus} onChange={(e) => setGradeBminus(e.target.value)} />
               </div>
               <div>
                 <p>Grade C+:</p>
-                <input type="number" placeholder="Grade C+" />
+                <input type="text" placeholder="Grade C+" value={gradeCplus} onChange={(e) => setGradeCplus(e.target.value)} />
               </div>
               <div>
                 <p>Grade C:</p>
-                <input type="number" placeholder="Grade C" />
+                <input type="text" placeholder="Grade C" value={gradeC} onChange={(e) => setGradeC(e.target.value)} />
               </div>
               <div>
                 <p>Grade C-:</p>
-                <input type="number" placeholder="Grade C-" />
+                <input type="text" placeholder="Grade C-" value={gradeCminus} onChange={(e) => setGradeCminus(e.target.value)} />
               </div>
               <div>
                 <p>Grade D+:</p>
-                <input type="number" placeholder="Grade D+" />
+                <input type="text" placeholder="Grade D+" value={gradeDplus} onChange={(e) => setGradeDplus(e.target.value)} />
               </div>
               <div>
                 <p>Grade D:</p>
-                <input type="number" placeholder="Grade D" />
+                <input type="text" placeholder="Grade D" value={gradeD} onChange={(e) => setGradeD(e.target.value)} />
               </div>
               <div>
                 <p>Grade D-:</p>
-                <input type="number" placeholder="Grade D-" />
+                <input type="text" placeholder="Grade D-" value={gradeDminus} onChange={(e) => setGradeDminus(e.target.value)} />
               </div>
               <div>
                 <p>Grade E:</p>
-                <input type="number" placeholder="Grade E" />
+                <input type="text" placeholder="Grade E" value={gradeE} onChange={(e) => setGradeE(e.target.value)} />
               </div>
             </div>
-          </div>
+            <button type="submit" className="btn">Upload Grades</button>
+          </form>
         </div>
       </section>
     </div>
