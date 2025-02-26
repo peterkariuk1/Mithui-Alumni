81ea6722895cc76c64fda66e0f9cc1b757b0499f
V1n-T3ch <vincegichomo704@gmail.com>
2025-02-27 02:25:06 +0300

    Added Authentication

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,29 +1,53 @@
 import SVGAnimation from "../images/undraw_profile_details_re_ch9r.svg";
 import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Logged in successfully");
+      navigate("/")
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setEmail("");
+      setPassword("");
+    }
+  }
+
   return (
     <section className="login-section">
-      <div className="left-section">
-        <h1>Mithui Alumni Association</h1>
-        <h2>Welcome Back!</h2>
-        <p className="login-instruction">Log in to your account</p>
-        <div className="username-container">
-          <p>Username</p>
-          <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <div className="left-section">
+          <h1>Mithui Alumni Association</h1>
+          <h2>Welcome Back!</h2>
+          <p className="login-instruction">Log in to your account</p>
+          <div className="username-container">
+            <p>Email</p>
+            <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+          </div>
+          <div className="password-container">
+            <p>Password</p>
+            <input className="password-input" type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+          </div>
+          <button className="login-button">
+            <p>Log In</p>
+          </button>
+          <p className="sign-up-instructions">
+            Dont have an account?<Link style={{ textDecoration: "none" }} to="/register"> Register
+            </Link>
+          </p>
         </div>
-        <div className="password-container">
-          <p>Password</p>
-          <input className="password-input" type="password" />
-        </div>
-        <button className="login-button">
-          <p>Log In</p>
-        </button>
-        <p className="sign-up-instructions">
-          Dont have an account?<Link style={{ textDecoration: "none" }} to="/register"> Register
-          </Link>
-        </p>
-      </div>
+      </form>
       <div className="right-section">
         <img src={SVGAnimation} />
       </div>
